9b47cd7c3d7a9d242315d7d0002e93076d469ffa
denizbeyza <byz.dnz0@gmail.com>
2022-06-14 23:12:52 +0300

    splash screen and icon

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:recipe_keep/blocs/recipe/recipe_bloc.dart';
 import 'package:recipe_keep/blocs/shopping_list/shopping_list_bloc.dart';
@@ -15,8 +16,10 @@ import 'package:recipe_keep/pages/auth/login_page.dart';
 import 'package:flutter/services.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await Firebase.initializeApp();
+  
   runApp(MultiBlocProvider(
     providers: [
       BlocProvider(
@@ -31,6 +34,8 @@ void main() async {
     ],
     child: const MyApp(),
   ));
+  FlutterNativeSplash.remove();
+
 }
 
 class MyApp extends StatefulWidget {
